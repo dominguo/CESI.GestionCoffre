60657406808c925637eb916365679b2f20c2a85c
etienne.lasseube@viacesi.fr <etienne.lasseube@viacesi.fr>
2016-09-14 16:06:45 +0200

    Ajout de la classe Frais

---
+++ javacesi/Frais.java
@@ -0,0 +1,17 @@
+package javacesi;
+import java.util.Date;
+import java.util.Vector;
+
+public class Frais {
+
+  public Integer IdFrais;
+
+  public Date DateAnnee;
+
+  public Float Montant;
+  
+  public String Description;
+
+    //public Vector<OGroupementBanque> myGroupementBanque;
+
+}
