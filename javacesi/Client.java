ac1b5a3aa3cfc3a97df710767c2d50bdc920638c
Valzul <paulbigeon@hotmail.fr>
2016-09-14 16:32:56 +0200

    Ajout de la classe Client

---
+++ javacesi/Client.java
@@ -0,0 +1,68 @@
+package javacesi;
+
+/**
+ * Created by Emna, Julien & Paul on 14/09/2016.
+ */
+
+public class Client {
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	private Agence agenceRattachement;
+	
+	// Constructeurs
+	public Client(int id, String nm, String pn, Agence agr) {
+		this.id = id;
+		this.nom = nm;
+		this.prenom = pn;
+		this.agenceRattachement = agr;
+	}
+	
+	public Client() {
+		
+	}
+	
+	// Getter
+    public int getId()
+    {
+        return this.id;
+    }	
+    
+    public String getNom()
+    {
+        return this.nom;
+    }	
+    
+    public String getPrenom()
+    {
+        return this.prenom;
+    }	
+    
+    public Agence getAgenceRattachement()
+    {
+        return this.agenceRattachement;
+    }	
+    
+	// Setter
+    public void setId(int id)
+    {
+        this.id = id;
+    }	
+    
+    public void setNom(String nm)
+    {
+        this.nom = nm;
+    }	
+    
+    public void setPrenom(String pn)
+    {
+        this.prenom = pn;
+    }	
+    
+    public void setAgenceRattachement(Agence agr)
+    {
+        this.agenceRattachement = agr;
+    }	
+    
+}
