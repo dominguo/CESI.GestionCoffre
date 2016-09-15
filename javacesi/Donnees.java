f5444e055277b67f59957a5036b589a92800c7b8
Valzul <paulbigeon@hotmail.fr>
2016-09-15 11:12:05 +0200

    Réencodage UTF8 sans BOM

--- javacesi/Donnees.java
+++ javacesi/Donnees.java
@@ -9,9 +9,9 @@ public class Donnees {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Jeux de données
+		// Jeux de donnÃ©es
 		
-		// Création d'une liste d'agences
+		// CrÃ©ation d'une liste d'agences
 		Agence a1 = new Agence();
 		Agence a2 = new Agence();
 		Agence a3 = new Agence();
@@ -21,7 +21,7 @@ public class Donnees {
 		Agence a7 = new Agence();
 		Agence a8 = new Agence();
 		
-		// Création de plusieurs ArrayList(s) d'agences
+		// CrÃ©ation de plusieurs ArrayList(s) d'agences
 		ArrayList<Agence> arla1 = new ArrayList<Agence>();
 		arla1.add(a1);
 		arla1.add(a2);
@@ -36,7 +36,7 @@ public class Donnees {
 		arla3.add(a7);
 		arla3.add(a8);
 		
-		// Création d'une liste de secteurs
+		// CrÃ©ation d'une liste de secteurs
 		Secteur s1 = new Secteur("Nancy",54000,"France");
 		s1.AddAgence(a1);
 		s1.AddAgence(a2);
@@ -51,7 +51,7 @@ public class Donnees {
 		s1.AddAgence(a7);
 		s1.AddAgence(a8);
 		
-		// Création d'une liste de banques
+		// CrÃ©ation d'une liste de banques
 		Banque b1 = new Banque("BANQUE POPULAIRE");
 		b1.AddSecteur(s1);
 		Banque b2 = new Banque("BANQUE LCL");
@@ -59,30 +59,30 @@ public class Donnees {
 		Banque b3 = new Banque("BANQUE BNP PARIBAS");
 		b1.AddSecteur(s3);
 		
-		// Création & association de la liste de banque au groupement de banque
+		// CrÃ©ation & association de la liste de banque au groupement de banque
 		GroupementBanque grpb = new GroupementBanque();
 		grpb.AddBanque(b1);
 		grpb.AddBanque(b2);
 		grpb.AddBanque(b3);
 		
-		// Création d'une liste de frais
+		// CrÃ©ation d'une liste de frais
 		Frais fr1 = new Frais();
 		Frais fr2 = new Frais();
 		Frais fr3 = new Frais();
 		
-		// Création d'une liste de coffres
+		// CrÃ©ation d'une liste de coffres
 		Coffre cf1 = new Coffre(1,"1er Etage","Armoire forte","Acier 50L",null,500000,null);
 		Coffre cf2 = new Coffre(2,"2eme Etage","Armoire forte","Acier 50L",null,25000,null);
 		Coffre cf3 = new Coffre(3,"2eme Etage","Armoire forte","Acier 50L",null,0,null);
 		Coffre cf4 = new Coffre(4,"2eme Etage","Armoire forte","Acier 50L",null,20,null);
 		Coffre cf5 = new Coffre(5,"3eme Etage","Armoire forte","Acier 50L",null,0.99,null);
 		
-		// Création d'une liste de clients
-		ClientBanque cl1 = new ClientBanque(1,"Schmit","Aurélien",a1,cf1);
+		// CrÃ©ation d'une liste de clients
+		ClientBanque cl1 = new ClientBanque(1,"Schmit","AurÃ©lien",a1,cf1);
 		ClientBanque cl2 = new ClientBanque(2,"Castro","Fidel",a2,cf2);
 		ClientBanque cl3 = new ClientBanque(3,"Trump","Donald",a5,cf4);
 		
-		// Création de dates
+		// CrÃ©ation de dates
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 		String d1 = "01/01/2015";
 		String d2 = "12/05/2012";
@@ -110,12 +110,12 @@ public class Donnees {
 			e.printStackTrace();
 		}
 
-		// Création des frais
-		Frais fra1 = new Frais(1,d1e,(float) 200.0,"Contrôle des huissiers");
+		// CrÃ©ation des frais
+		Frais fra1 = new Frais(1,d1e,(float) 200.0,"ContrÃ´le des huissiers");
 		Frais fra2 = new Frais(2,d2e,(float) 100.0,"Taxe");
-		Frais fra3 = new Frais(3,d3e,(float) 50.0,"Contrôle de routine");
+		Frais fra3 = new Frais(3,d3e,(float) 50.0,"ContrÃ´le de routine");
 		
-		// Création des opérations
+		// CrÃ©ation des opÃ©rations
 		Operation op1 = new Operation();
 		op1.setFrs(fra1);
 		Operation op2 = new Operation();
@@ -124,7 +124,7 @@ public class Donnees {
 		op3.setFrs(fra3);
 		Operation op4 = new Operation();
 		
-		// Création d'une liste de banque
+		// CrÃ©ation d'une liste de banque
 		ArrayList<Banque> bql = new ArrayList<Banque>();
 		bql.add(b1);
 		bql.add(b2);
